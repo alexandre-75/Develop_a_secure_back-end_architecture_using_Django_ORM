19280bdc63fea2ad61a389d825788213c8791808
Alexandre SASSY <sassy.alexandre@gmail.com>
2023-04-25 15:10:18 +0200

    custom CustomerAdmin

--- source/customers/admin.py
+++ source/customers/admin.py
@@ -6,3 +6,7 @@ from .models import Client
 class CustomerAdmin(admin.ModelAdmin):
     
     list_display =["first_name", "last_name", "status_client", "email", "phone", "company", "sales_client"]
+    search_fields = ["first_name", "last_name", "email", "status_client"]
+    list_filter = ["status_client"]
+    list_per_page = 5
+    ordering = ["last_name"]
--- source/customers/models.py
+++ source/customers/models.py
@@ -19,9 +19,7 @@ class Client(models.Model):
     date_updated = models.DateTimeField(auto_now=True)
     
     sales_client = models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL, related_name='sales_client')
-    
-    class Meta:
-        ordering = ["-date_created"]
+
 
     def __str__(self):
             return (f" {self.first_name} {self.last_name}  ")

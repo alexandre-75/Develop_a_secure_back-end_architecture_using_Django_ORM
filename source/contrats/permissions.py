f410b3fe436cb9f990b6e5cfa25ff1e5d4ee249d
Alexandre SASSY <sassy.alexandre@gmail.com>
2023-04-27 17:40:24 +0200

    permissions contracts

--- source/contrats/permissions.py
+++ source/contrats/permissions.py
@@ -1,12 +1,31 @@
 from rest_framework import permissions
-from .models import Contract
+from django.db import connection
 
 class ContractPermissions(permissions.BasePermission):
-    
-     def has_permission(self, request, view):   
-        if request.user.role == "SALE":
+           
+    def has_permission(self, request, view): 
+ 
+        user_id = request.user.id
+        group_name = None
+        
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """
+                SELECT auth_group.name
+                FROM auth_group
+                INNER JOIN accounts_user_groups
+                ON auth_group.id = accounts_user_groups.group_id
+                WHERE accounts_user_groups.user_id = %s
+                """,[user_id]
+                )
+            results = cursor.fetchall()
+
+        for row in results:
+            group_name = row[0]
+            
+        if group_name == "SALES_TEAM":
             return request.method in ["GET", "POST", "PUT"]
-        elif request.user.role == "SUPPORT":
+        elif group_name == "SUPPORT_TEAM":
             return request.method in ["GET"]
         else:
             return request.method in ["GET", "POST", "PUT", "DELETE"]
--- source/contrats/views.py
+++ source/contrats/views.py
@@ -3,29 +3,35 @@ from .serializers import ContractListSerializer, ContractDetailSerializer
 from .models import Contract
 from events.models import Event
 from rest_framework.permissions import IsAuthenticated
+from .permissions import ContractPermissions
+
+
+def get_contracts_queryset(user):
+    if user.role == "SALE":
+        return Contract.objects.filter(sales_contract_id=user.id)
+    elif user.role == "SUPPORT":
+        list_events = Event.objects.filter(support_events=user.id)
+        list_contracts = [event.contract_events_id for event in list_events]
+        return Contract.objects.filter(id__in=list_contracts)
+    else:
+        return Contract.objects.all()
 
 
 class ContractList(ListAPIView):
     
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, ContractPermissions]
     serializer_class = ContractListSerializer
+    
     def get_queryset(self):
         user = self.request.user
-        if user.role =="SALE":
-            return Contract.objects.filter(sales_contract_id=user.id)
-        elif user.role == "SUPPORT":
-            list_contracts = []
-            list_events = Event.objects.filter(support_events=user.id)
-            for i in list_events:
-                list_contracts.append(i.contract_events_id)
-            return Contract.objects.filter(id__in=list_contracts)       
-        else:
-            return Contract.objects.all()
-
-
+        return get_contracts_queryset(user)
+        
 
 class ContractDetail(RetrieveAPIView, UpdateAPIView, DestroyAPIView):
     
-    permission_classes = [IsAuthenticated]
-    queryset = Contract.objects.all()
+    permission_classes = [IsAuthenticated, ContractPermissions]
     serializer_class = ContractDetailSerializer
+    
+    def get_queryset(self):
+        user = self.request.user
+        return get_contracts_queryset(user)

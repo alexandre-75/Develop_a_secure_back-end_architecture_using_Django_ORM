33fd0f045109f2175742ebad8ccfd086a707106a
Alexandre SASSY <sassy.alexandre@gmail.com>
2023-04-27 11:28:26 +0200

    def get_queryset

--- source/customers/views.py
+++ source/customers/views.py
@@ -28,6 +28,19 @@ class CustomersList(ListAPIView, CreateAPIView):
 class CustomersDetail(RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView):
     
     permission_classes = [IsAuthenticated, ClientPermissions]
-    queryset = Client.objects.all()
     serializer_class = CustomersDetailSerializer
+    
+    def get_queryset(self):
+        user = self.request.user
+        if user.role == "SALE":
+            return Client.objects.filter(sales_client=user)     
+        elif user.role == "SUPPORT":
+            list_clients = []
+            list_events = Event.objects.filter(support_events=user.id)
+            for i in list_events:
+                list_clients.append(i.client_events_id)
+            return Client.objects.filter(id__in=list_clients)
+        else: 
+            return Client.objects.all()
+    
     

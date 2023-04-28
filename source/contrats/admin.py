133831f0569be720026f69d3198bf3c5abe55ddc
Alexandre SASSY <sassy.alexandre@gmail.com>
2023-04-28 10:30:03 +0200

    view admin/ filter contracts

--- source/contrats/admin.py
+++ source/contrats/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Contract
+from events.models import Event
 
 
 
@@ -10,3 +11,14 @@ class ContractAdmin(admin.ModelAdmin):
     list_per_page = 5
     ordering = ["-date_created"]
     list_filter = ["contract_status"]
+    
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        user = request.user
+        if user.groups.filter(name="SALES_TEAM").exists():
+            queryset = queryset.filter(sales_contract_id=user)
+        elif user.groups.filter(name="SUPPORT_TEAM").exists():
+            user_events = Event.objects.filter(support_events_id=user)
+            client_ids = user_events.values_list('contract_events_id', flat=True)
+            queryset = queryset.filter(id__in=client_ids)
+        return queryset

26967b1e55d3d3c42bea1d26d358a5584b288a9b
Alexandre SASSY <sassy.alexandre@gmail.com>
2023-04-29 17:58:49 +0200

    add filter by first_name, email, amount, date_created

--- source/contrats/views.py
+++ source/contrats/views.py
@@ -1,20 +1,14 @@
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 from .serializers import ContractListSerializer, ContractDetailSerializer
 from .models import Contract
+from customers.models import Client
 from events.models import Event
 from rest_framework.permissions import IsAuthenticated
 from .permissions import ContractPermissions
+from django.db.models import Q
 
 
-def get_contracts_queryset(user):
-    if user.role == "SALE":
-        return Contract.objects.filter(sales_contract_id=user.id)
-    elif user.role == "SUPPORT":
-        list_events = Event.objects.filter(support_events=user.id)
-        list_contracts = [event.contract_events_id for event in list_events]
-        return Contract.objects.filter(id__in=list_contracts)
-    else:
-        return Contract.objects.all()
+
 
 
 class ContractList(ListAPIView):
@@ -23,9 +17,43 @@ class ContractList(ListAPIView):
     serializer_class = ContractListSerializer
     
     def get_queryset(self):
-        user = self.request.user
-        return get_contracts_queryset(user)
         
+        user = self.request.user
+        
+        client_name = self.request.query_params.get('first_name')
+        client_email = self.request.query_params.get('email')
+        amount_contract = self.request.query_params.get('amount')
+        date_created_contract = self.request.query_params.get('date_created')
+        
+        queryset = Contract.objects.all()
+        
+        if user.role == "SALE":
+            queryset = Contract.objects.filter(sales_contract_id=user.id)
+            if amount_contract:
+                queryset = queryset.filter(amount__icontains=amount_contract) 
+            elif date_created_contract:
+                queryset = queryset.filter(date_created__icontains=date_created_contract) 
+            elif client_email:
+                queryset = queryset.filter(client_contract_id__email__icontains=client_email)
+            elif client_name:
+                queryset = queryset.filter(client_contract_id__first_name__icontains=client_name)
+            else:
+                pass      
+        elif user.role == "SUPPORT":
+            list_events = Event.objects.filter(support_events=user.id)
+            list_contracts = [event.contract_events_id for event in list_events]
+            queryset = Contract.objects.filter(id__in=list_contracts)
+            if amount_contract:
+                queryset = queryset.filter(amount__icontains=amount_contract) 
+            elif date_created_contract:
+                queryset = queryset.filter(date_created__icontains=date_created_contract) 
+            elif client_email:
+                queryset = queryset.filter(client_contract_id__email__icontains=client_email)
+            elif client_name:
+                queryset = queryset.filter(client_contract_id__first_name__icontains=client_name)
+            else:
+                pass 
+        return queryset
 
 class ContractDetail(RetrieveAPIView, UpdateAPIView, DestroyAPIView, CreateAPIView):
     
@@ -33,5 +61,14 @@ class ContractDetail(RetrieveAPIView, UpdateAPIView, DestroyAPIView, CreateAPIVi
     serializer_class = ContractDetailSerializer
     
     def get_queryset(self):
+
         user = self.request.user
-        return get_contracts_queryset(user)
+        queryset = Contract.objects.all()
+
+        if user.role == "SALE":
+            queryset = Contract.objects.filter(sales_contract_id=user.id)
+        elif user.role == "SUPPORT":
+            list_events = Event.objects.filter(support_events=user.id)
+            list_contracts = [event.contract_events_id for event in list_events]
+            return Contract.objects.filter(id__in=list_contracts)
+        return queryset

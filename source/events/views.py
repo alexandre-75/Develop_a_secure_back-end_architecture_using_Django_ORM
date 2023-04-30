38dba1b51030cb74213d2ffb5565b7ab544876d5
Alexandre SASSY <sassy.alexandre@gmail.com>
2023-04-30 14:07:03 +0200

    search first_name/email/event_date

--- source/events/views.py
+++ source/events/views.py
@@ -12,12 +12,35 @@ class EventList(ListAPIView):
     
     def get_queryset(self):
         user = self.request.user
+        print(user)
+        
+        client_name = self.request.query_params.get('first_name')
+        client_email = self.request.query_params.get('email')
+        date_created_event = self.request.query_params.get('event_date')
+        
+        queryset = Event.objects.all()
+        
         if user.role == "SALE":
-            return Event.objects.filter(sales_events=user.id)
+            queryset = Event.objects.filter(sales_events=user.id)
+            if date_created_event:
+                queryset = queryset.filter(event_date__icontains=date_created_event)
+            elif client_name:
+                queryset = queryset.filter(client_event_id__first_name__icontains=client_email)
+            elif client_email:
+                queryset = queryset.filter(client_event_id__email__icontains=client_email)
+            else:
+                pass
         elif user.role == "SUPPORT":
-            return Event.objects.filter(support_events=user.id)
-        else:
-            return Event.objects.all()
+            queryset = Event.objects.filter(support_events=user.id)
+            if date_created_event:
+                queryset = queryset.filter(event_date__icontains=date_created_event)
+            elif client_name:
+                queryset = queryset.filter(client_event_id__first_name__icontains=client_email)
+            elif client_email:
+                queryset = queryset.filter(client_event_id__email__icontains=client_email)
+            else:
+                pass
+        return queryset
    
 
 class EventDetail(RetrieveAPIView, UpdateAPIView, DestroyAPIView, CreateAPIView):
